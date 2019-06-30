65680fb9e3df99a01b964c99963885620bba433f
swdev.smz@gmail.com <swdev.smz@gmail.com>
2019-06-30 22:59:38 +0900

    コメント追記

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ def hello_world():
     # 顔検出
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        #顔にマーキング
+        # 顔に長方形の描画
         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        # roi_gray = gray[y:y + h, x:x + w]
-        # roi_color = img[y:y + h, x:x + w]
-        # eyes = eye_cascade.detectMultiScale(roi_gray)
-        # for (ex, ey, ew, eh) in eyes:
-        #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        # 目検出
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     # 画像を表示
     cv2.imshow('img', img)
